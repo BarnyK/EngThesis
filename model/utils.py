2f43031e3a396a0242a607267b7adb2f7c7fb57c
BarnyK <barnaba.krupowicz@gmail.com>
2022-03-31 19:35:44 +0200

    Added choose_device function to utils

--- model/utils.py
+++ model/utils.py
@@ -1,4 +1,7 @@
 from torch import nn
+import torch
+from zmq import device
+
 
 def conv2d_norm(in_, out, kernel, stride, padding=0, dilation=1) -> nn.Sequential:
     return nn.Sequential(
@@ -28,3 +31,11 @@ def conv3d_norm_relu(in_, out, kernel, stride, padding=0, dilation=1):
         nn.BatchNorm3d(out),
         nn.ReLU(inplace=True),
     )
+
+
+def choose_device(cpu: bool):
+    if cpu:
+        return torch.device("cpu")
+    if torch.cuda.is_available():
+        return torch.device("cuda")
+    raise Exception("Pytorch did not find cuda")
