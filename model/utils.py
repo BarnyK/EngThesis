fixed dataloader scaling values too low, basic training

model/utils.py
@@ -44,11 +44,10 @@ def choose_device(cpu: bool):
     raise Exception("Pytorch did not find cuda")
 
 
-def save_model(model: Net,optimizer:Optimizer,max_disp:int,savepath:str):
+def save_model(model: Net,optimizer:Optimizer,savepath:str):
     res = {
         "model":model.state_dict(),
         "optimizer":optimizer.state_dict(),
-        "max_disp":max_disp
     }
     torch.save(res,savepath)
     return res
@@ -56,4 +55,4 @@ def save_model(model: Net,optimizer:Optimizer,max_disp:int,savepath:str):
 
 def load_model(loadpath:str):
     state = torch.load(loadpath)
-    return state["model"],state["optimizer"], state["max_disp"]
+    return state["model"],state["optimizer"]

procedures/evaluation_mode.py
@@ -1,6 +1,8 @@
 import os
 import time
 from importlib.resources import path
+from PIL import Image
+import numpy as np
 
 import torch
 from data.dataset import check_paths_exist, pad_image, pad_image_reverse
@@ -69,7 +71,7 @@ def evaluate(
 
     if disparity_image and os.path.exists(disparity_image):
         gt = read_file(disparity_image, disparity=True)
-        gt = to_tensor(gt).to(device)
+        gt = to_tensor(gt).to(device).float()/256
         if gt.shape == disp.shape:
             print("EPE:", error_epe(gt, disp))
             print("3p:", error_3p(gt, disp))
@@ -79,8 +81,11 @@ def evaluate(
                 "Can't create measures if output disparity is different shape than ground truth"
             )
 
-    disp_image = transforms.ToPILImage("L")(disp)
-
+    
+    disp.squeeze_(0)
+    res = np.array(disp.cpu(),dtype=np.uint8)
+    disp_image = Image.fromarray(res)
+    # disp_image = transforms.ToPILImage()(disp)
     os.makedirs(os.path.dirname(result_image), exist_ok=True)
     if not result_image.lower().endswith(".png"):
         result_image += ".png"

procedures/test_mode.py
@@ -1,5 +1,7 @@
+import enum
+from data.dataset import DisparityDataset
 from data.indexes import index_set
-
+from torch.utils.data import DataLoader
 
 def test_indexes(args: dict):
     try:
@@ -16,3 +18,9 @@ def test_indexes(args: dict):
     except TypeError as er:
         print(er)
 
+def test_loader(args: dict):
+    train, _ = index_set(**args)
+    trainset = DisparityDataset(train,random_crop=False)
+    trainloader = DataLoader(trainset,1,shuffle=False,num_workers=0,pin_memory=True)
+    for i,(l,r,d) in enumerate(trainloader):
+        print(d.mean())

procedures/train_mode.py
@@ -1,13 +1,15 @@
 from statistics import mode
 
 import torch
-from data.dataset import DisparityDataset
+from tqdm import tqdm
+from data.dataset import DisparityDataset, pad_image, pad_image_reverse
 from data.indexes import index_set
 from torch.utils.data import DataLoader
 from time import time
+from measures.measures import error_3p, error_epe
 from model.utils import choose_device,load_model,save_model
 from model import Net
-
+import torch.nn.functional as F
 def train(epochs,batch_size,learning_rate,dataset_name,cpu,max_disp,load_file,save_file,**kwargs):
     # Index set
     # Create Dataset
@@ -22,35 +24,73 @@ def train(epochs,batch_size,learning_rate,dataset_name,cpu,max_disp,load_file,sa
     trainset = DisparityDataset(trainset)
     testset = DisparityDataset(testset,random_crop=False)
 
-    trainloader = DataLoader(trainset,batch_size,shuffle=True,num_workers=1,pin_memory=True)
-    testloader = DataLoader(testset,batch_size,shuffle=False,num_workers=1,pin_memory=False)
+    trainloader = DataLoader(trainset,batch_size,shuffle=True,num_workers=2,pin_memory=True)
+    testloader = DataLoader(testset,1,shuffle=False,num_workers=2,pin_memory=False)
         
     net = Net(max_disp).to(device)
-    net2 = Net(256)
-
-    net.load_state_dict(net2.state_dict())
-
-    optimizer = torch.optim.Adam(net.parameters(),learning_rate)
-
+    
     if load_file:
-        model_state, optim_state, loaded_max_disp = load_model(load_file)
+        model_state, optim_state = load_model(load_file)
+        net.load_state_dict(model_state)
+    
+    optimizer = torch.optim.Adam(net.parameters(),learning_rate)
+    if load_file:
+        optimizer.load_state_dict(optim_state)
+        if learning_rate >= 0:
+            optimizer.param_groups[0]['lr'] = learning_rate
 
-
-
-
-    net.train()
+    scaler = torch.cuda.amp.GradScaler()
 
     for epoch in range(epochs):
         st = time()
-        for i,(left,right,gt) in enumerate(trainloader):
+        net.train()
+        rl = 0.0
+        for i,(left,right,gt) in tqdm(enumerate(trainloader),total=len(trainloader)):
+            optimizer.zero_grad()
             left = left.to(device)
             right = right.to(device)
             gt = gt.to(device)
-            
-            d1,d2,d3 = net(left,right)  
+            with torch.cuda.amp.autocast():
+                d1,d2,d3 = net(left,right) 
+                mask = gt > 0
+                loss = (
+                    0.5 * F.smooth_l1_loss(d1[mask], gt[mask], size_average=True)
+                    + 0.7 * F.smooth_l1_loss(d2[mask], gt[mask], size_average=True)
+                    + F.smooth_l1_loss(d3[mask], gt[mask], size_average=True)
+                )
+            scaler.scale(loss).backward()
+            loss.backward()
+            optimizer.step()
+            rl += loss.item()
+        et = time()
 
+        if save_file:
+            save_model(net,optimizer,save_file+f"-{epoch}")
 
-    st = time()
-      
-    et = time()
-    print(et-st,"pin_memory=True")
+        
+        epe_sum = 0
+        e3p_sum = 0
+        for i,(left,right,gt) in tqdm(enumerate(testloader)):
+            net.eval()
+            left = left.to(device)
+            right = right.to(device)
+            gt = gt.to(device)
+            left,og = pad_image(left)
+            right,og = pad_image(right)
+            with torch.no_grad():
+                with torch.cuda.amp.autocast():
+                    d = net(left,right)
+                    d = pad_image_reverse(d,og)
+                    epe = error_epe(gt,d)
+                    e3p = error_3p(gt,d)
+            epe_sum += epe
+            e3p_sum += e3p
+        epe_avg = epe_sum/(i+1)
+        e3p_avg = e3p_sum/(i+1)
+        print(f"Epoch {epoch} out of {epochs}")
+        print("Took: ", round(et-st,2))
+        print("RL: ",rl/i)
+        print("EPE: ",epe_avg)
+        print("E3P: ",e3p_avg)
+    if save_file:
+        save_model(net,optimizer,save_file)