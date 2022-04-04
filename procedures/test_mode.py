5d5e20c585131793355733f67211a51d22e329f6
BarnyK <barnaba.krupowicz@gmail.com>
2022-04-04 11:00:53 +0200

    added print_validation

--- procedures/test_mode.py
+++ procedures/test_mode.py
@@ -6,8 +6,22 @@ from torch.utils.data import DataLoader
 def test_indexes(args: dict):
     try:
         train, test = index_set(**args)
-        print(f"Dataset {args.get('name')!r} total images: {len(train)+len(test)} training: {len(train)}, test: {len(test)}")
-        print("Test succeeded")
+        print(f"Dataset {args.get('dataset_name')!r} total images: {len(train)+len(test)} training: {len(train)}, test: {len(test)}")
+    except FileNotFoundError as er:
+        print(er)
+        print("The path specified is incorrect or the data inside is not as expected")
+    except ValueError as er:
+        print(er)
+    except KeyError as er:
+        print(er)
+    except TypeError as er:
+        print(er)
+
+def print_validation(args: dict):
+    try:
+        train, test = index_set(**args)
+        for t in test:
+            print(t[2])
     except FileNotFoundError as er:
         print(er)
         print("The path specified is incorrect or the data inside is not as expected")
