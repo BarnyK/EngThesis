d945580d4cc1c26992b1b521086de08081b16891
BarnyK <barnaba.krupowicz@gmail.com>
2022-04-05 12:19:03 +0200

    Added init_scale to gradscale and manual seed

--- procedures/train_mode.py
+++ procedures/train_mode.py
@@ -8,6 +8,7 @@ from data.utils import pad_image, pad_image_reverse
 from measures import error_3p, error_epe
 from model import Net
 from model.utils import choose_device, load_model, save_model
+from torch.cuda.amp import GradScaler, autocast
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
@@ -26,6 +27,7 @@ def train(
     **kwargs,
 ):
     # torch.autograd.set_detect_anomaly(True)
+    torch.manual_seed(0)
     if save_file is not None:
         save_filename = path.basename(save_file)
         if log_file and path.isdir(log_file):
@@ -67,13 +69,13 @@ def train(
         model_state, optim_state, scaler_state = load_model(load_file)
         net.load_state_dict(model_state)
 
-    optimizer = torch.optim.Adam(net.parameters(), learning_rate)
+    optimizer = torch.optim.Adam(net.parameters(), learning_rate, eps=1e-6)
     if load_file:
         optimizer.load_state_dict(optim_state)
         if learning_rate >= 0:
             optimizer.param_groups[0]["lr"] = learning_rate
 
-    scaler = torch.cuda.amp.GradScaler()
+    scaler = GradScaler(init_scale=256, enabled=not cpu)
     if load_file and scaler_state is not None:
         scaler.load_state_dict(scaler_state)
 
@@ -86,27 +88,30 @@ def train(
         for i, (left, right, gt) in tqdm(
             enumerate(trainloader), total=len(trainloader)
         ):
-            
+            optimizer.zero_grad(False)
             left = left.to(device)
             right = right.to(device)
+            mask = gt > 0
             gt = gt.to(device)
-            with torch.cuda.amp.autocast():
+
+            with autocast(enabled=not cpu):
                 d1, d2, d3 = net(left, right)
-                mask = gt > 0
                 loss = (
                     0.5 * F.smooth_l1_loss(d1[mask], gt[mask])
                     + 0.7 * F.smooth_l1_loss(d2[mask], gt[mask])
                     + F.smooth_l1_loss(d3[mask], gt[mask])
                 )
-            epe_sum += error_epe(gt, d3) * d3.shape[0]
-            e3p_sum += error_3p(gt, d3) * d3.shape[0]
-            optimizer.zero_grad(True)
             scaler.scale(loss).backward()
             scaler.step(optimizer)
             scaler.update()
-            rl += loss.item()
+
+            # Metrics
+            epe_sum += error_epe(gt, d3) * d3.shape[0]
+            e3p_sum += error_3p(gt, d3) * d3.shape[0]
+            rl += loss.item() * d3.shape[0]
 
         train_time = time() - st
+        rl_avg = rl / len(trainset)
         epe_avg = epe_sum / len(trainset)
         e3p_avg = e3p_sum / len(trainset)
 
@@ -115,7 +120,7 @@ def train(
 
         print(f"Epoch {epoch+1} out of {epochs}")
         print("Took: ", round(train_time, 2), "seconds")
-        print("Running loss: ", rl / i)
+        print("Running loss: ", rl_avg)
         print("EPE: ", epe_avg)
         print("3p error: ", e3p_avg)
 
@@ -147,7 +152,7 @@ def train(
             print("Stats on test set:")
             print("EPE: ", epe_avg_t)
             print("E3P: ", e3p_avg_t)
-        save_log(epoch, train_time, rl, epe_avg, e3p_avg, epe_avg_t, e3p_avg_t)
+        save_log(epoch, train_time, rl_avg, epe_avg, e3p_avg, epe_avg_t, e3p_avg_t)
 
     if save_file:
         save_model(net, optimizer, scaler, save_file)
