2a87d9a2a568ff9b52fd556004f94ff2d96b43dd
BarnyK <barnaba.krupowicz@gmail.com>
2022-04-06 23:01:56 +0200

    fixes measures to incluse max_disp

--- measures/measures.py
+++ measures/measures.py
@@ -7,6 +7,7 @@ from time import time
 def error_3p(
     ground_truth: torch.Tensor,
     disparity: torch.Tensor,
+    max_disp: int,
     tau: Tuple[int, float] = (3, 0.05),
 ) -> float:
     """
@@ -17,14 +18,14 @@ def error_3p(
     Taken from Kitty2015 evaluation metric
     """
     pixel_diff, percent_diff = tau
-    mask = ground_truth > 0
+    mask = torch.logical_and(ground_truth <= max_disp, ground_truth > 0)
     E = abs(disparity - ground_truth)[mask]
     n_err = len(E[(E > pixel_diff) & ((E / abs(ground_truth[mask])) > percent_diff)])
     return n_err / len(E)
 
 
 @torch.jit.script
-def error_epe(ground_truth: torch.Tensor, disparity: torch.Tensor) -> float:
-    mask = ground_truth > 0
+def error_epe(ground_truth: torch.Tensor, disparity: torch.Tensor, max_disp: int) -> float:
+    mask = torch.logical_and(ground_truth < max_disp, ground_truth > 0)
     res = torch.abs(disparity - ground_truth)[mask]
     return res.mean().item()
