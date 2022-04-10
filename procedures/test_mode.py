fa9c70d6eb4287c78dc75014e2e002f5e2a4c980
Barnaba Krupowicz <01138338@pw.edu.pl>
2022-04-11 00:06:01 +0200

    redibe test-mode, wrote some functions

--- procedures/test_mode.py
+++ procedures/test_mode.py
@@ -56,15 +56,13 @@ def test_loader(args: dict):
     from tqdm import tqdm
     import torch
 
-    train, _ = index_set(**args)
-    trainset = DisparityDataset(train, random_crop=False)
-    trainloader = DataLoader(trainset, 1, shuffle=False, num_workers=2, pin_memory=True)
-    for i, (l, r, d) in tqdm(enumerate(trainloader), total=len(trainloader)):
-        if torch.min(d).item() <= 0:
-            print(torch.min(d))
-        if torch.isnan(l).any():
-            print("nan detected", i)
-        if torch.isnan(r).any():
-            print("nan detected", i)
-        if torch.isnan(d).any():
-            print("nan detected", i)
+    train, test = index_set(**args)
+    trainset = DisparityDataset(train, random_crop=False,return_paths=True)
+    testset = DisparityDataset(test, random_crop=False,return_paths=True)
+    trainloader = DataLoader(trainset, 1, shuffle=False, num_workers=2, pin_memory=False)
+    testloader = DataLoader(testset, 1, shuffle=False, num_workers=2, pin_memory=False)
+
+    for loader in [trainloader, testloader]:
+        for i, (l, r, d, p) in tqdm(enumerate(loader), total=len(loader)):
+            pass
+    print("Successfully loaded all files")
